b4b29c97bc5e5abb0263bc05156339fb557c14f4
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2020-11-09 20:56:23 +0100

    second

---
+++ src/sources.js
@@ -0,0 +1,22 @@
+ export const sources = {
+    cd: 'assets/PC.png',
+    komora: 'assets/komora2.png',
+    backImg: 'assets/BG-kotlownia-01.png',
+    ps: 'assets/PS.png',
+    pe: 'assets/PE.png',
+    pp: 'assets/PP.png',
+    pvc: 'assets/PVC.png',
+    pet: 'assets/PET.png',
+    doorClosed: 'assets/drzwi_zamkniete.png',
+    doorOpened: 'assets/drzwi_otwarte.png',
+    pcpop: 'assets/PC-pop.png',
+    pspop: 'assets/PS-pop.png',
+    pepop: 'assets/PE-pop.png',
+    pppop: 'assets/PP-pop.png',
+    pvcpop: 'assets/PVC-pop.png',
+    petpop: 'assets/PET-pop.png',
+    finalBackImage: 'assets/BG-dach-01.png',
+    resetButtonEndImage: 'assets/button-reset-END.png',
+    startBackImage: 'assets/BG-Start.png',
+    startButtonImage: 'assets/button-START.png'
+};
